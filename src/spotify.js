537a844158a99aa50284b9d6185bc93910eff3cf
bunnie26 <66902288+bunnie26@users.noreply.github.com>
2021-07-21 19:15:09 +0530

    🚧 work in progresss

---
+++ src/spotify.js
@@ -0,0 +1,28 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const clientId = "e8e54d22ba024ad9831c7db699a80568";
+const redirectUri = "http://localhost:3000/";
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+// pulling access token from url
+
+export const getTokenFromUrl = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
