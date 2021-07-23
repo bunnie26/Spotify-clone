7203f90b9f6e5c0d6e3f417aabf91aac17ab13b4
bunnie26 <66902288+bunnie26@users.noreply.github.com>
2021-07-23 23:02:45 +0530

    ✨ added new styles

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Sidebar.css";
 import SidebarOption from "./SidebarOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryIconIcon from "@material-ui/icons/LibraryMusic";
 
 function Sidebar() {
   return (
@@ -9,9 +12,9 @@ function Sidebar() {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         className="sidebar__logo"
       />
-      <SidebarOption title="Home" />
-      <SidebarOption title="Search" />
-      <SidebarOption title="Your Library" />
+      <SidebarOption icon = {HomeIcon}title="Home" />
+      <SidebarOption icon = {SearchIcon} title="Search" />
+      <SidebarOption icon = {LibraryIconIcon} title="Your Library" />
     </div>
   );
 }
--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./SidebarOption.css";
 
-function SidebarOption({ title, Icons }) {
-  return( <div className="sidebarOption">
-      <p>{title}</p>
-  </div>
-  )
+function SidebarOption({ title, Icon }) {
+  return (
+    <div className="sidebarOption">
+      {Icon}
+      {Icon ? <h4>{title}</h4> : <p>{title}</p>}
+    </div>
+  );
 }
 
 export default SidebarOption;
