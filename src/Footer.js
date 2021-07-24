16070fb4953d43a2c600873b33a6526525636ec7
bunnie26 <66902288+bunnie26@users.noreply.github.com>
2021-07-24 12:41:08 +0530

    ⚡️ made some changes in side bar

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,46 @@
 import React from "react";
 import "./Footer.css";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import { Grid, Slider } from "@material-ui/core";
 
 function Footer() {
   return (
     <div className="footer">
-      <h1>i am footer</h1>
+      <div className="footer__left">
+        <img src="" className="footer__albumLogo" />
+        <div className="footer__songInfo">
+          <h4>Rockstar</h4>
+          <p>Post Malone</p>
+        </div>
+      </div>
+
+      <div className="footer__center">
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon className="footer__icon" />
+        <PlayCircleOutlineIcon className="footer__icon" fontSize="large" />
+        <SkipNextIcon className="footer__icon" />
+        <RepeatIcon className="footer__green" />
+      </div>
+
+      <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,17 +4,27 @@ import SidebarOption from "./SidebarOption";
 import HomeIcon from "@material-ui/icons/Home";
 import SearchIcon from "@material-ui/icons/Search";
 import LibraryIconIcon from "@material-ui/icons/LibraryMusic";
+import { useDataLayerValue } from "./DataLayer";
 
 function Sidebar() {
+  const [{ playlists }, dispatch] = useDataLayerValue();
   return (
     <div className="sidebar">
       <img
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         className="sidebar__logo"
       />
-      <SidebarOption icon = {HomeIcon}title="Home" />
-      <SidebarOption icon = {SearchIcon} title="Search" />
-      <SidebarOption icon = {LibraryIconIcon} title="Your Library" />
+      <SidebarOption Icon={HomeIcon} title="Home" />
+      <SidebarOption Icon={SearchIcon} title="Search" />
+      <SidebarOption Icon={LibraryIconIcon} title="Your Library" />
+
+      <br />
+      <strong className="sidebar__title">PLAYLISTS</strong>
+      <hr />
+
+      {playlists?.items?.map((playlist) => (
+        <SidebarOption title={playlist.name} />
+      ))}
     </div>
   );
 }
